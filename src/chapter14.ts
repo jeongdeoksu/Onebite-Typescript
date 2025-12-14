960e11911fe7922ed5577a959aca71fcd84fb1c8
jeongdeoksu <jeongdeoksu@gmail.com>
2025-12-14 18:46:42 +0900

    14. 객체

---
+++ src/chapter14.ts
@@ -0,0 +1,22 @@
+// object (객체리터럴 타입)
+// 구조적 타입 시스템 (반대는 명목적 타입 시스템 예 JAVA)
+// Property Based Type System
+let user: {
+  id?: number; // ? 옵셔널 프로퍼티
+  name: string;
+} = {
+  id: 1,
+  name: 'John',
+};
+
+user = {
+  name: '홍길동',
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: 'MY API KEY',
+};
+
+config.apiKey = 'MY API KEY';
